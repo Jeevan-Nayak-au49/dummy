9a99c72c089d735c86bb644b0c41003c7c3893d2
Sanjib <chiranjeebrout12@gmail.com>
2022-12-17 02:08:12 +0530

    .

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const ejs = require('ejs')
 const path = require('path')
 
 const expressLayout = require('express-ejs-layouts')
+const { CallTracker } = require('assert')
 
 const PORT = process.env.PORT || 3000
 
@@ -14,15 +15,20 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('public'))
 
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
 
 
 // set template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('coustomers/cart.ejs')
+})
 
 
 app.listen(PORT, () =>{
